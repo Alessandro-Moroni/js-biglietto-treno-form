319e9b1b00c46ad04b8db153bb91d91c45dc536a
Alessandro-Moroni <alemoro98@gmail.com>
2024-02-21 17:28:02 +0100

    [finish]ticket

--- js/script.js
+++ js/script.js
@@ -1,23 +1,8 @@
-let distance = parseInt('Quanti chilometri devi percorrere?');
-console.log(distance);
-let age = parseInt('Quanti anni hai?')
-console.log(age);
 const costDistance = 0.21;
-
-let costTicket = distance + costDistance;
-
-
 const bt1 = document.querySelector('.bt1');
+const bt2 = document.querySelector('.bt2');
 const ticketContainer = document.querySelector('.ticket-container');
 const selectAge = document.querySelector('.select-age')
-const sale = document.getElementById('sale')
-
-
-if(age < 18){
-  costTicket = ((costTicket / 100) * 80);
-}else if(age > 65){
-  costTicket = ((costTicket / 100) * 60);
-}
 
 
 
@@ -29,17 +14,32 @@ bt1.addEventListener('click',function(){
 
   document.getElementById('name').innerHTML = textName
   // chilometri
-  let km = document.getElementById('km').value;
+  let km = parseInt(document.getElementById('km').value);
 
   document.getElementById('km-tot').innerHTML = km
 
+
   // età
-  selectAge.addEventListener('change',function()){
-    sale.innerHTML = 'sei nella fascia ' + selectAge.value;
-    console.log(sale);
+  let age = selectAge.value
+  console.log(age);
+  document.getElementById('sale').innerHTML = age
+
+  let costTicket = km * costDistance;
+  if(age === 'under'){
+  costTicket = ((costTicket / 100) * 80);
+  }else if(age === 'over'){
+    costTicket = ((costTicket / 100) * 60);
   }
+console.log(costTicket);
+document.getElementById('price').innerHTML = costTicket.toFixed(2) + ' €'
   
 })
 
 
+bt2.addEventListener('click', function(){
+  ticketContainer.classList.add('d-none')
+  textName.value = '';
+  km.value = '';
+  selectAge.value = '';
 
+})
